2fdde9b432ad5eac7539b46120d09c2cd4dae44f
Prawinraj <112323984+Prawin-r@users.noreply.github.com>
2025-03-25 19:10:46 +0530

    Update flight.service.ts

--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, map, Observable, of, tap} from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Flight } from '../models/flight.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FlightService {
-  private apiUrl = 'https://gist.githubusercontent.com/Prawin-r/a80c7a98e2d53ceed3e3ed1144e119f0/raw/bd0e76d9fdcca5a0ca0ddb4b76a1b63fc6534929/api.json'; // ✅ Corrected path
+  private apiUrl =
+    'https://gist.githubusercontent.com/Prawin-r/a80c7a98e2d53ceed3e3ed1144e119f0/raw/bd0e76d9fdcca5a0ca0ddb4b76a1b63fc6534929/api.json'; // ✅ Corrected path
 
   constructor(private http: HttpClient) {}
 
-   getFlights(): Observable<Flight[]> {
+  getFlights(): Observable<Flight[]> {
     return this.http.get<{ flights: Flight[] }>(this.apiUrl).pipe(
       tap((data) => {
         console.log('Fetched flights:', data);
